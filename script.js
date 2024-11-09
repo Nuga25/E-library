5ba61210c069c120058646cfd74d6a6df8249d63
Nuga25 <osinugaifeoluwa1@gmail.com>
2024-11-09 17:10:29 +0100

    functions that enable user add books to library through a form works

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ function Books(title, author, pages, read) {
   this.read = read;
 }
 
-//function to create new book objects and add to array
+//function to create new book objects and add to array(myLibrary)
 function addBookToLibrary(title, author, pages, read) {
   const book = new Books(title, author, pages, read);
   myLibrary.push(book);
   return book;
 }
 
-//manually added books to library to test
-let book1 = addBookToLibrary("hello world", "ifeoluwa", 200, "yes");
-let book3 = addBookToLibrary("hey pookie", "tiktok", 280, "yes");
-let book2 = addBookToLibrary("Dark Vanessa", "Nuga", 150, "not read");
+//function to display last book entered
+function displayBook() {
+  const container = document.querySelector("#container");
+  const card = document.querySelector(".card-template");
 
-//function to display each book
-function displayEachBook() {
-  myLibrary.forEach((book) => {
-    const container = document.querySelector("#container");
-    const card = document.querySelector(".card-template");
-    const clone = document.importNode(card.content, true);
+  //get the last book from myLibrary
+  const book = myLibrary[myLibrary.length - 1];
+
+  /*check if there is a book to display*/
+  if (book) {
+    const clone = document.importNode(card.content, true); //clone template
     const bookTitle = clone.querySelector(".title");
     const bookAuthor = clone.querySelector(".author");
     const bookPages = clone.querySelector(".pages");
@@ -38,6 +38,39 @@ function displayEachBook() {
     bookStatus.textContent = book.read;
 
     container.appendChild(clone);
-  });
+  }
 }
-displayEachBook();
+
+//add a click event listener on 'add book button' to display a dialog to add books
+const dialog = document.querySelector("#dialog");
+const addBookButton = document.querySelector("#add-book");
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+//close dialog
+const closeDialog = document.querySelector(".close-dialog");
+closeDialog.addEventListener("click", () => {
+  dialog.close();
+});
+
+//button that submits form and adds book to library
+const submitFormButton = document.querySelector("#submit-book-btn");
+submitFormButton.addEventListener("click", (e) => {
+  e.preventDefault(); //to prevent default behaviour of submit button
+  dialog.close();
+
+  const title = document.querySelector("#form-title");
+  const author = document.querySelector("#form-author");
+  const pages = document.querySelector("#form-pages");
+  const read = document.querySelector("#form-read-status");
+
+  addBookToLibrary(title.value, author.value, pages.value, read.value);
+  displayBook();
+
+  //clear input fields after "Add to library" button is clicked
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.value = "";
+});
